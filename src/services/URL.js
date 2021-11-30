dc7b935a39b5f97a88f4f011d26c2c8d1b35baad
mateuskuritza <mateuskuritza@gmail.com>
2021-11-30 10:25:52 -0300

    Preparando revisão de código

--- src/services/URL.js
+++
@@ -1,6 +0,0 @@
-const URL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:4000"
-        : "https://apigratibox.herokuapp.com";
-
-export default URL;
--- src/utils/storedUser.js
+++
@@ -1,8 +0,0 @@
-function getStoredUser() {
-  return JSON.parse(localStorage.getItem('user'));
-}
-function storeUser(user) {
-  localStorage.setItem('user', JSON.stringify(user));
-}
-
-export { getStoredUser, storeUser };
